58f3095b67e90c796eb30429fce3f17ea34b7d62
cz <1046802917@qq.com>
2020-03-27 17:59:36 +0800

    整合ftl

--- src/main/java/per/cz/controller/ActivitiController.java
+++ src/main/java/per/cz/controller/ActivitiController.java
@@ -6,16 +6,19 @@ import org.activiti.engine.history.HistoricActivityInstance;
 import org.activiti.engine.repository.ProcessDefinition;
 import org.activiti.engine.repository.ProcessDefinitionQuery;
 import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.runtime.ProcessInstanceQuery;
 import org.activiti.engine.task.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import per.cz.entity.Holiday;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 流程业务
@@ -38,6 +41,48 @@ public class ActivitiController {
 
     @Autowired
     private RepositoryService repositoryService;
+
+    /**
+     * <p>启动请假流程</p>
+     * @return String 流程实例ID
+     * @author FRH
+     * @time 2018年12月10日上午11:03:36
+     * @version 1.0
+     */
+    @RequestMapping(value="/start")
+    @ResponseBody
+    public String start() {
+        // xml中定义的ID
+        String instanceKey = "myHoliday6";
+        log.info("开启请假流程...");
+
+        // 设置流程参数，开启流程
+        Map<String,Object> map = new HashMap<>();
+        Holiday holiday = new Holiday();
+        holiday.setUserName("joker");
+        holiday.setNum(5F);
+        map.put("holiday",holiday);
+
+        //使用流程定义的key启动流程实例，key对应helloworld.bpmn文件中id的属性值，使用key值启动，默认是按照最新版本的流程定义启动
+        ProcessInstance instance = runtimeService.startProcessInstanceByKey(instanceKey, map);
+
+        log.info("启动流程实例成功:{}", instance);
+        log.info("流程实例ID:{}", instance.getId());
+        log.info("流程定义ID:{}", instance.getProcessDefinitionId());
+
+
+        //验证是否启动成功
+        //通过查询正在运行的流程实例来判断
+        ProcessInstanceQuery processInstanceQuery = runtimeService.createProcessInstanceQuery();
+        //根据流程实例ID来查询
+        List<ProcessInstance> runningList = processInstanceQuery.processInstanceId(instance.getProcessInstanceId()).list();
+        log.info("根据流程ID查询条数:{}", runningList.size());
+
+        // 返回流程ID
+        return instance.getId();
+    }
+
+
 
     /**
      * 启动流程
